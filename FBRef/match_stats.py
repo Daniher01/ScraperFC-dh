1488e594e36138cfc046985d3aaadf79fdfa27f3
Daniel Hernandez <daniher02@gmail.com>
2024-02-13 16:51:47 -0300

    se obtienen stats de los partidos

--- FBRef/match_stats.py
+++ FBRef/match_stats.py
@@ -7,11 +7,16 @@ from io import StringIO
 scraper = sfc.FBRef()
 try:
     # Scrape the match using the FBRef match link
-    link = 'https://fbref.com/en/matches/967efd56/Southampton-Tottenham-Hotspur-September-20-2020-Premier-League'
+    link = 'https://fbref.com/en/matches/ed48c4b3/Univ-Chile-Huachipato-January-23-2023-Primera-Division'
     match = scraper.scrape_match(link=link)
+    
+    # Local
     stats_home = match['Home Player Stats'].values[0]['Summary'][0]
+    # Visitante
+    stats_away = match['Away Player Stats'].values[0]['Summary'][0]
     # Guardar la data en csv
     stats_home.to_csv(f'data/FBRef/match_home.csv', index=False, sep=';', encoding='utf-8')
+    stats_away.to_csv(f'data/FBRef/match_away.csv', index=False, sep=';', encoding='utf-8')
 except:
     # Catch and print any exceptions.
     traceback.print_exc()
