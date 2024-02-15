532775cdfe3251a0de28c4b986b508e15b931663
Daniel Hernandez <daniher02@gmail.com>
2024-02-15 16:00:02 -0300

    se agrega la variable rating de sofascore

--- ScraperFC-2.9.2/ScraperFC/Sofascore.py
+++ ScraperFC-2.9.2/ScraperFC/Sofascore.py
@@ -46,7 +46,7 @@ class Sofascore:
             'dribbledPast', 'bigChancesMissed', 'totalShots', 'shotsOnTarget',
             'blockedShots', 'goalConversionPercentage', 'hitWoodwork', 
             'offsides', 'expectedGoals', 'errorLeadToGoal', 'errorLeadToShot',
-            'passToAssist'
+            'passToAssist', 'rating'
         ]
         self.concatenated_fields = "%2C".join(self.league_stats_fields)
     
